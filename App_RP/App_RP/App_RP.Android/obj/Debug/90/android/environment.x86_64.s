982fcd2dfc2a3e3d6661370373d56297b94e56fc
rp01 <rp.reservaciones@gmail.com>
2020-01-24 17:35:53 -0600

    add changes

--- App_RP/App_RP/App_RP.Android/obj/Debug/90/android/environment.x86_64.s
+++ App_RP/App_RP/App_RP.Android/obj/Debug/90/android/environment.x86_64.s
@@ -62,7 +62,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"89350e32-03cd-4b55-ab74-5792c31eb83f"
+	.asciz	"4cca2d0f-277b-4bc8-81fe-dd03e8de9dd4"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
